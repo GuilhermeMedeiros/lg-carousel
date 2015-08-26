8d05fc58620d7f8806b47f0c61ea69d9c031795d
Guilherme Medeiros <guilherme@backbase.com>
2015-08-26 21:13:42 +0200

    add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,19 @@
 'use strict';
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: './dist',
+        filename: 'main.bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: 'style!css'},
+            {test: /\.js?$/, loader: 'babel', exclude: /(node_modules)/}
+        ]
+    },
+
+    devServer: {
+       hot: true
+   }
+};
